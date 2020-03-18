a3aa391c83343e6b68dfd5ecbf9d9cfbe0a09588
MyeongchanJin <jmcchan@naver.com>
2020-03-18 20:32:44 +0900

    유효성 체크로 불가능한 이메일 중복체크용 코드

---
+++ src/main/java/com/npeeproject/api/member/ValidCustomException.java
@@ -0,0 +1,30 @@
+package com.npeeproject.api.member;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@ResponseStatus(HttpStatus.BAD_REQUEST)
+public class ValidCustomException extends RuntimeException {
+
+    private Error[] errors;
+
+    public ValidCustomException(String defaultMessage, String field) {
+        this.errors = new Error[]{new Error(defaultMessage, field)};
+    }
+
+    @AllArgsConstructor
+    @NoArgsConstructor
+    @Getter
+    public static class Error {
+
+        private String defaultMessage;
+        private String field;
+    }
+
+}
