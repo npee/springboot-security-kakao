6e4534f57039b04d72d5a4cc9ab8eb8c514f6899
MyeongchanJin <jmcchan@naver.com>
2020-03-18 19:04:30 +0900

    회원 정보 저장, 회원 목록 불러오는 로직

---
+++ src/main/java/com/npeeproject/api/member/MemberService.java
@@ -0,0 +1,33 @@
+package com.npeeproject.api.member;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+public class MemberService {
+
+    private MemberRepository memberRepository;
+
+    public MemberService(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
+
+    @Transactional
+    public Long save(MemberRequestDto memberRequestDto) {
+        return memberRepository.save(memberRequestDto.toEntity()).getId();
+    }
+
+    @Transactional(readOnly = true)
+    public List<MemberResponseDto> findAll() {
+        return memberRepository
+                .findAll()
+                .stream()
+                .map(MemberResponseDto::new)
+                .collect(Collectors.toList());
+    }
+
+
+}
