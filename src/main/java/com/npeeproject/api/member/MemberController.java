b2dd96a8a91e3f15ff6a5cdbd71171adf08f8483
MyeongchanJin <jmcchan@naver.com>
2020-03-18 19:05:28 +0900

    각 로직의 역할에 맞는 URL을 매칭해주는 Controller 생성

---
+++ src/main/java/com/npeeproject/api/member/MemberController.java
@@ -0,0 +1,30 @@
+package com.npeeproject.api.member;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
+import java.util.List;
+
+@RestController
+public class MemberController {
+
+    private MemberService memberService;
+
+    public MemberController(MemberService memberService) {
+        this.memberService = memberService;
+    }
+
+    @PostMapping("/member")
+    public Long saveMember(@RequestBody @Valid MemberRequestDto memberRequestDto) {
+        return memberService.save(memberRequestDto);
+    }
+
+    @GetMapping("/members")
+    public List<MemberResponseDto> findAll() {
+        return memberService.findAll();
+    }
+}
