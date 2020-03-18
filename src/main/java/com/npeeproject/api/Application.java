534081070755ce6e6f6baca88b1229dea2f0a983
MyeongchanJin <jmcchan@naver.com>
2020-03-18 20:34:02 +0900

    DefaultErrorAttributes에서 추가

--- src/main/java/com/npeeproject/api/Application.java
+++ src/main/java/com/npeeproject/api/Application.java
@@ -1,12 +1,37 @@
 package com.npeeproject.api;
 
+import com.npeeproject.api.member.ValidCustomException;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.error.DefaultErrorAttributes;
+import org.springframework.boot.web.servlet.error.ErrorAttributes;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.context.request.WebRequest;
+
+import java.util.Map;
 
 @SpringBootApplication
 public class Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
+    }
+
+    @Bean
+    public ErrorAttributes errorAttributes() {
+        return new DefaultErrorAttributes() {
+
+            @Override
+            public Map<String, Object> getErrorAttributes(WebRequest webRequest, boolean includeStackTrace) {
+
+                Map<String, Object> errorAttributes = super.getErrorAttributes(webRequest, includeStackTrace);
+                Throwable error = getError(webRequest);
+                if (error instanceof ValidCustomException) {
+                    errorAttributes.put("errors", ((ValidCustomException)error).getErrors());
+                }
+                //return super.getErrorAttributes(webRequest, includeStackTrace);
+                return errorAttributes;
+            }
+        };
     }
 }
