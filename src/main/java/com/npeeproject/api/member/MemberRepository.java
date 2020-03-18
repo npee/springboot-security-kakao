cee13698075d756e19b908c61399ccec56f5f309
MyeongchanJin <jmcchan@naver.com>
2020-03-18 19:02:54 +0900

    JpaRepository 상속받은 Repository 인터페이스

---
+++ src/main/java/com/npeeproject/api/member/MemberRepository.java
@@ -0,0 +1,9 @@
+package com.npeeproject.api.member;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface MemberRepository extends JpaRepository<Member, Long> {
+    Optional<Member> findByEmail(String email);
+}
