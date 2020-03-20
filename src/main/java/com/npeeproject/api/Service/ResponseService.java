b028aa1a90de896cc305527dde9d1ae393e1eea9
MyeongchanJin <jmcchan@naver.com>
2020-03-20 21:17:20 +0900

    응답시 결과 값을 한번 더 추가해주는 메서드

---
+++ src/main/java/com/npeeproject/api/Service/ResponseService.java
@@ -0,0 +1,63 @@
+package com.npeeproject.api.Service;
+
+import com.npeeproject.api.model.response.CommonResult;
+import com.npeeproject.api.model.response.ListResult;
+import com.npeeproject.api.model.response.SingleResult;
+import lombok.Getter;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class ResponseService {
+
+    @Getter
+    public enum CommonResponse {
+        SUCCESS(0, "성공"),
+        FAIL(-1, "실패");
+
+        int code;
+        String message;
+
+        CommonResponse(int code, String message) {
+            this.code = code;
+            this.message = message;
+        }
+    }
+    public <T> SingleResult<T> getSingleResult(T data) {
+        SingleResult<T> result = new SingleResult<>();
+        result.setData(data);
+        setSuccessResult(result);
+        return result;
+    }
+
+
+    public <T> ListResult<T> getListResult(List<T> list) {
+        ListResult<T> result = new ListResult<>();
+        result.setList(list);
+        setSuccessResult(result);
+        return result;
+    }
+
+    public CommonResult getSuccessResult() {
+        CommonResult result = new CommonResult();
+        setSuccessResult(result);
+        return result;
+    }
+
+    public CommonResult getFailResult() {
+        CommonResult result = new CommonResult();
+        result.setSuccess(false);
+        result.setCode(CommonResponse.FAIL.getCode());
+        result.setMessage(CommonResponse.FAIL.getMessage());
+        return result;
+    }
+
+    private void setSuccessResult(CommonResult result) {
+        result.setSuccess(true);
+        result.setCode(CommonResponse.SUCCESS.getCode());
+        result.setMessage(CommonResponse.SUCCESS.getMessage());
+    }
+
+
+}
