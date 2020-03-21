feed8684e0ae98e9edb062598290985525372267
MyeongchanJin <jmcchan@naver.com>
2020-03-22 05:19:52 +0900

    예외 커스터마이징에 사용할 클래스 (RuntimeException 상속)

---
+++ src/main/java/com/npeeproject/api/advice/exception/MemberNotFoundException.java
@@ -0,0 +1,14 @@
+package com.npeeproject.api.advice.exception;
+
+public class MemberNotFoundException extends RuntimeException {
+    public MemberNotFoundException() {
+    }
+
+    public MemberNotFoundException(String message) {
+        super(message);
+    }
+
+    public MemberNotFoundException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
