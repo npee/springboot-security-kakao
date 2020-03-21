8ad214acb2484249881704583567111a81178940
MyeongchanJin <jmcchan@naver.com>
2020-03-22 05:18:40 +0900

    특정회원 조회 실패시 예외처리 설정하는 클래스

---
+++ src/main/java/com/npeeproject/api/advice/ExceptionAdvice.java
@@ -0,0 +1,24 @@
+package com.npeeproject.api.advice;
+
+import com.npeeproject.api.model.response.config.CommonResult;
+import com.npeeproject.api.service.ResponseService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import javax.servlet.http.HttpServletRequest;
+
+@RequiredArgsConstructor
+@RestControllerAdvice(basePackages = "com.npeeproject.api")
+public class ExceptionAdvice {
+
+    private final ResponseService responseService;
+
+    @ExceptionHandler(Exception.class)
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    protected CommonResult defaultException(HttpServletRequest request, Exception e) {
+        return responseService.getFailResult();
+    }
+}
