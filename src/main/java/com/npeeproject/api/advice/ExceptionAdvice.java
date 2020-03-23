68014cab109dc6ef6f27c624e4349d3683193cab
MyeongchanJin <jmcchan@naver.com>
2020-03-23 22:52:06 +0900

    출력되는 Message가 Locale에 맞는 Message로 변경되도록 수정. 기본값은 unKnown 배정

--- src/main/java/com/npeeproject/api/advice/ExceptionAdvice.java
+++ src/main/java/com/npeeproject/api/advice/ExceptionAdvice.java
@@ -4,6 +4,8 @@ import com.npeeproject.api.advice.exception.MemberNotFoundException;
 import com.npeeproject.api.model.response.config.CommonResult;
 import com.npeeproject.api.service.ResponseService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -17,17 +19,26 @@ public class ExceptionAdvice {
 
     private final ResponseService responseService;
 
-    /*
-    @ExceptionHandler(Exception.class)
-    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
-    protected CommonResult defaultException(HttpServletRequest request, Exception e) {
-        return responseService.getFailResult();
-    }
-    */
+    private final MessageSource messageSource;
+
 
     @ExceptionHandler(Exception.class)
     @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    protected CommonResult defaultException(HttpServletRequest request, Exception e) {
+        return responseService.getFailResult(Integer.valueOf(getMessage("unKnown.code")), getMessage("unKnown.message"));
+    }
+
+    @ExceptionHandler(MemberNotFoundException.class)
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
     protected CommonResult memberNotFountException(HttpServletRequest request, MemberNotFoundException e) {
-        return responseService.getFailResult();
+        return responseService.getFailResult(Integer.valueOf(getMessage("memberNotFound.code")), getMessage("memberNotFound.message"));
+    }
+
+    private String getMessage(String code) {
+        return getMessage(code, null);
+    }
+
+    private String getMessage(String code, Object[] args) {
+        return messageSource.getMessage(code, args, LocaleContextHolder.getLocale());
     }
 }
