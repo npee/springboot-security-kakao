92a99c0c4a4cd2dbade519cd4fd12eb4ce07427f
MyeongchanJin <jmcchan@naver.com>
2020-03-25 22:13:42 +0900

    Member Dto만 사용하도록 수정

--- src/main/java/com/npeeproject/api/service/MemberService.java
+++ src/main/java/com/npeeproject/api/service/MemberService.java
@@ -3,15 +3,9 @@ package com.npeeproject.api.service;
 import com.npeeproject.api.advice.exception.MemberNotFoundException;
 import com.npeeproject.api.advice.exception.ValidCustomException;
 import com.npeeproject.api.model.Member;
-import com.npeeproject.api.model.request.MemberRequestDto;
-import com.npeeproject.api.model.response.MemberResponseDto;
 import com.npeeproject.api.repository.MemberRepository;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-
-import java.util.List;
-import java.util.Optional;
-import java.util.stream.Collectors;
 
 @Service
 public class MemberService {
@@ -23,10 +17,10 @@ public class MemberService {
     }
 
     @Transactional
-    public MemberResponseDto save(MemberRequestDto memberRequestDto) {
+    public Member save(Member member) {
 
-        verifyDuplicateEmail(memberRequestDto.getEmail(), "email");
-        return new MemberResponseDto(memberRepository.save(memberRequestDto.toEntity()));
+        verifyDuplicateEmail(member.getEmail(), "email");
+        return memberRepository.save(member);
     }
 
     private void verifyDuplicateEmail(String email, String field) {
@@ -36,32 +30,11 @@ public class MemberService {
         }
     }
 
-    @Transactional(readOnly = true)
-    public List<MemberResponseDto> findAll() {
-
-        return memberRepository
-                .findAll()
-                .stream()
-                .map(MemberResponseDto::new)
-                .collect(Collectors.toList());
-    }
-
     @Transactional
-    public MemberResponseDto update(MemberRequestDto memberRequestDto) {
+    public Member update(Member member) {
 
-        verifyDuplicateEmail(memberRequestDto.getEmail(), "update-email");
-        return new MemberResponseDto(memberRepository.save(memberRequestDto.toEntity(memberRequestDto.getId())));
-    }
-
-    @Transactional(readOnly = true)
-    public MemberResponseDto findById(Long id) {
-
-        Optional<Member> member = memberRepository.findById(id);
-        if (member.isPresent()) {
-            return member.map(MemberResponseDto::new).get();
-        } else {
-            return new MemberResponseDto(member.orElseThrow(MemberNotFoundException::new));
-        }
+        verifyDuplicateEmail(member.getEmail(), "update-email");
+        return memberRepository.save(member);
     }
 
     @Transactional
