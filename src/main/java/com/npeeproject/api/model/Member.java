c2370071c68bda9c226874c5d51c28d71f52c5c6
MyeongchanJin <jmcchan@naver.com>
2020-03-31 23:56:15 +0900

    코드 정리

--- src/main/java/com/npeeproject/api/model/Member.java
+++ src/main/java/com/npeeproject/api/model/Member.java
@@ -7,7 +7,7 @@ import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
 import javax.persistence.*;
-import javax.validation.constraints.Email;
+
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Pattern;
